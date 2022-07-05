d2888a0d678a6ca2e66eb70db5da7bc322dd5ce0
Richard Goshen <rick.goshen@gmail.com>
2022-07-04 22:44:27 -0700

    wip get data from api

--- src/movies/MoviesList.js
+++ src/movies/MoviesList.js
@@ -2,27 +2,42 @@ import { useState, useEffect } from 'react';
 import { Movie } from './Movie';
 import { Filter } from '../Filter';
 
-const movies = [
-  {
-    name: '36th Chamber',
-  },
-  {
-    name: 'Man of Iron',
-  },
-  {
-    name: '5 Deadly Venoms',
-  },
-  {
-    name: 'Iron Man',
-  },
-];
+// const movies = [
+//   {
+//     name: '36th Chamber',
+//   },
+//   {
+//     name: 'Man of Iron',
+//   },
+//   {
+//     name: '5 Deadly Venoms',
+//   },
+//   {
+//     name: 'Iron Man',
+//   },
+// ];
+
+const API_URL =
+  'https://api.themoviedb.org/3/discover/movie?api_key=9c32f397fdb4d58982d492124e77250a&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_monetization_types=flatrate';
 
 export function MoviesList() {
   const [filter, setFilter] = useState('');
+  const [movies, setMovies] = useState([]);
+
+  const getMovies = async () => {
+    try {
+      const res = await fetch(API_URL);
+      const movies = await res.json();
+      setMovies(movies.results);
+      console.log(movies);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   useEffect(() => {
-    console.log('hit effect');
-  }, [filter]);
+    getMovies();
+  }, []);
 
   return (
     <div>
