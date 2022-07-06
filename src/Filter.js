912de52368f92293e49bb242cdc06175ff788aa6
Richard Goshen <rick.goshen@gmail.com>
2022-07-05 20:01:22 -0700

    add class names

--- src/Filter.js
+++ src/Filter.js
@@ -1,10 +1,15 @@
 import PropTypes from 'prop-types';
+import './Filter.css';
 
 export function Filter({ filter, setFilter }) {
   return (
-    <label>
+    <label className='filter-label'>
       Filter:
-      <input value={filter} onChange={(e) => setFilter(e.target.value)} />
+      <input
+        value={filter}
+        onChange={(e) => setFilter(e.target.value)}
+        className='filter-input'
+      />
     </label>
   );
 }
