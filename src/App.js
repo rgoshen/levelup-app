ef677db57a4fb4c22f965ebe0034b88a196d0bc8
Richard Goshen <rick.goshen@gmail.com>
2022-07-06 10:07:24 -0700

    add page title and styling

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 function App() {
   return (
     <div className='App'>
+      <h1 className='App-title'>Movies List</h1>
       <Router>
         <Routes>
           <Route path='/' element={<MoviesList />} />
